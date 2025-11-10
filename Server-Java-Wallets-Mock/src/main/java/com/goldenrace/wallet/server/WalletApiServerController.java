3846691cc583ce2449534b3a13005a6f0a2f081d
“Miguel <miguel.portilla@golden-race.com>
2025-11-10 09:44:04 +0100

    testing with credit 1€

--- Server-Java-Wallets-Mock/src/main/java/com/goldenrace/wallet/server/WalletApiServerController.java
+++ Server-Java-Wallets-Mock/src/main/java/com/goldenrace/wallet/server/WalletApiServerController.java
@@ -34,7 +34,7 @@ public class WalletApiServerController implements WalletApi {
 
     private static final IAppLogger LOGGER = AppLogger.getLogger(WalletApiServerController.class);
 
-    private static double DEFAULT_CREDIT   = 0.0; //100_000D
+    private static double DEFAULT_CREDIT   = 1.0; //100_000D
     private static final String DEFAULT_CURRENCY = "EUR"; //BTC
 
     private static final boolean SOLVE_CREDIT = true;
